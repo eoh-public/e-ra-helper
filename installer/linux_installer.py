d5080d5ac3fa919ae1e29cb8e70f0391be025281
Bang Dao <bang@eoh.io>
2023-06-11 00:45:49 +0700

    Add print

--- installer/linux_installer.py
+++ installer/linux_installer.py
@@ -3,17 +3,16 @@ import os
 
 desktop_json_path = 'native-messaging-host/com.eoh.era_helper_desktop.json'
 
+print('register native message host with chrome')
 current_folder = os.path.realpath(os.path.dirname(__file__))
-# modify native message hot path
 with open(desktop_json_path, 'r') as f:
     desktop_json = json.load(f)
 
 desktop_json['path'] = os.path.join(current_folder, 'e-ra-helper')
 
-# register native message host with chrome
 with open(f'{os.environ["HOME"]}/.config/google-chrome/NativeMessagingHosts/com.eoh.era_helper_desktop.json', 'w') as f:
     json.dump(desktop_json, f, indent=4)
 
-# install chrome extension
+print('install chrome extension')
 chrome_extension_url = 'https://chrome.google.com/webstore/detail/e-ra-helper/ppihjimjhbokffindbgmmopjfcmhhelh'
 os.system(f'google-chrome {chrome_extension_url}')
