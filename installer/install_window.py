6742d68359e8cede92bef43cbdd0682e3be3186b
Bang Dao <bang@eoh.io>
2023-06-11 01:44:09 +0700

    Add vlc start on window

--- installer/install_window.py
+++ installer/install_window.py
@@ -11,8 +11,12 @@ with open(desktop_json_path, 'r') as f:
 
 desktop_json['path'] = os.path.join(current_folder, 'e-ra-helper')
 
-with open(f'{os.environ["HOME"]}/.config/google-chrome/NativeMessagingHosts/com.eoh.era_helper_desktop.json', 'w') as f:
+with open(f'{current_folder}/temp_com.eoh.era_helper_desktop.json', 'w') as f:
     json.dump(desktop_json, f, indent=4)
+
+os.system(
+    f'REG ADD "HKCU\\Software\\Google\\Chrome\\NativeMessagingHosts\\com.eoh.era_helper_desktop" '
+    f'/ve /t REG_SZ /d "{current_folder}\\temp_com.eoh.era_helper_desktop.json" /f')
 
 print('install chrome extension')
 chrome_extension_url = 'https://chrome.google.com/webstore/detail/e-ra-helper/ppihjimjhbokffindbgmmopjfcmhhelh'
--- native-messaging-host/e-ra-helper.py
+++ native-messaging-host/e-ra-helper.py
@@ -1,7 +1,10 @@
 import json
+import platform
 import subprocess
 
 import nativemessaging
+
+is_window = platform.system() == 'Windows'
 
 try:
     message = nativemessaging.get_message()
@@ -11,9 +14,13 @@ try:
 
     if message['type'] == 'open_in_vlc_player':
         # start vlc player
-        subprocess.Popen(['vlc', message['uri']], shell=True)
+        if is_window:
+            subprocess.Popen(['C:\\Program Files\\VideoLAN\\VLC\\vlc.exe', message['uri']], shell=True)
+        else:
+            subprocess.Popen(['vlc', message['uri']], shell=True)
         with open("log.txt", "a") as f:
             f.write("Start vlc\n")
+        nativemessaging.send_message(nativemessaging.encode_message('Start vlc'))
 except Exception as ex:
     with open("log.txt", "a") as f:
         f.write(str(ex) + "\n")
