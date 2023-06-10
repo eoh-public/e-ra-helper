12410d29ee023cd37c5d02ccac1c0ae1b6bfc00a
Bang Dao <bang@eoh.io>
2023-06-11 02:23:36 +0700

    fix wrong zip path installer for window

--- .github/scripts/make_window_zip.py
+++ .github/scripts/make_window_zip.py
@@ -2,5 +2,5 @@ from zipfile import ZipFile
 
 with ZipFile('window.zip', 'w') as zip_object:
    zip_object.write('dist/e-ra-helper/e-ra-helper.exe', 'e-ra-helper.exe')
-   zip_object.write('dist/linux_installer/window_installer.exe', 'window_installer.exe')
+   zip_object.write('dist/window_installer/window_installer.exe', 'window_installer.exe')
    zip_object.write('native-messaging-host/com.eoh.era_helper_desktop.json', 'com.eoh.era_helper_desktop.json')
