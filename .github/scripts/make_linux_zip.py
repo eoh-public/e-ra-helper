5af8d91a3e0b9f919b7be62a82c5d9b13b8ae3ea
Bang Dao <bang@eoh.io>
2023-06-11 02:08:59 +0700

    Add auto build linux

---
+++ .github/scripts/make_linux_zip.py
@@ -0,0 +1,7 @@
+import os
+from zipfile import ZipFile
+
+with ZipFile('linux.zip', 'w') as zip_object:
+   zip_object.write('dist/e-ra-helper/e-ra-helper', 'e-ra-helper')
+   zip_object.write('dist/linux_installer/linux_installer', 'linux_installer')
+   zip_object.write('native-messaging-host/com.eoh.era_helper_desktop.json', 'com.eoh.era_helper_desktop.json')
