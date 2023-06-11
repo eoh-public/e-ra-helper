3d72bf4707823cbc4188f46b2be6f4ef803dfffc
Bang Dao <bang@eoh.io>
2023-06-11 09:03:41 +0700

    Fix wrong zip file json path

--- .github/scripts/make_linux_zip.py
+++ .github/scripts/make_linux_zip.py
@@ -3,4 +3,4 @@ from zipfile import ZipFile
 with ZipFile('e-ra-helper-linux.zip', 'w') as zip_object:
    zip_object.write('dist/e-ra-helper', 'e-ra-helper')
    zip_object.write('dist/linux_installer', 'linux_installer')
-   zip_object.write('native-messaging-host/com.eoh.era_helper_desktop.json', 'com.eoh.era_helper_desktop.json')
+   zip_object.write('installer/com.eoh.era_helper_desktop.json', 'com.eoh.era_helper_desktop.json')
--- .github/scripts/make_window_zip.py
+++ .github/scripts/make_window_zip.py
@@ -3,4 +3,4 @@ from zipfile import ZipFile
 with ZipFile('e-ra-helper-window.zip', 'w') as zip_object:
    zip_object.write('dist/e-ra-helper.exe', 'e-ra-helper.exe')
    zip_object.write('dist/window_installer.exe', 'window_installer.exe')
-   zip_object.write('native-messaging-host/com.eoh.era_helper_desktop.json', 'com.eoh.era_helper_desktop.json')
+   zip_object.write('installer/com.eoh.era_helper_desktop.json', 'com.eoh.era_helper_desktop.json')
