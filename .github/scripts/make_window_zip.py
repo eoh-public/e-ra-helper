26bf95bdba838f01b02958ce7b1f467757501dd0
Bang Dao <bang@eoh.io>
2024-11-24 10:43:43 +0700

    Fix build production

--- .github/scripts/make_window_zip.py
+++ .github/scripts/make_window_zip.py
@@ -4,5 +4,5 @@ with ZipFile('e-ra-helper-window.zip', 'w') as zip_object:
    zip_object.write('dist/e-ra-helper.exe', 'e-ra-helper.exe')
    zip_object.write('dist/window_installer.exe', 'window_installer.exe')
    zip_object.write('dist/ttlock_encoder.exe', 'ttlock_encoder.exe')
-   zip_object.write('ffmpeg/ffmpeg.exe', 'ffmpeg.exe')
+   zip_object.write('ffmpeg/ffmpeg-7.1-essentials_build/bin/ffmpeg.exe', 'ffmpeg.exe')
    zip_object.write('installer/com.eoh.era_helper_desktop.json', 'com.eoh.era_helper_desktop.json')
